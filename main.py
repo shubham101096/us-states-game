5b4e21c5c29e516635778e9b5bdb9ac6c0eef539
shubham101096 <shubhammishra10@gmail.com>
2021-05-26 17:59:05 +0530

    store states to learn in a csv file

--- main.py
+++ main.py
@@ -24,9 +24,19 @@ list_y = data["y"].to_list()
 print(list_state)
 
 score = 0
+guessed_states = []
+missed_states = []
 
 while is_on and score <50:
-    state = screen.textinput(title=f"State game {score}/50", prompt="enter another state:").title()
+    state = screen.textinput(title=f"State game {score}/50", prompt="enter another state: (enter 'exit' to exit)").title()
+    if state == "Exit":
+        for state in list_state:
+            if state not in guessed_states:
+                missed_states.append(state)
+        dict = {"states":missed_states}
+        df = pandas.DataFrame(dict)
+        df.to_csv("states_to_learn.csv")
+        break
     if state in list_state:
         index = list_state.index(state)
         timmy.penup()
@@ -34,6 +44,7 @@ while is_on and score <50:
         timmy.pendown()
         timmy.write(state, align="center", font=("Arial", 8, "normal"))
         score += 1
+        guessed_states.append(state)
 
 
 
